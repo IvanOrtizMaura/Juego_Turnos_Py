a18516231bfbb009802f006f36ce6d45d8cdc0a1
IvanOrtizMaura <Ivantiz45@gmail.com>
2022-05-20 03:13:10 +0200

    20/05/2022

--- CPU.py
+++ CPU.py
@@ -1,85 +1,124 @@
-from random import random
-from Manu import jugar, personajeCPU
-import os
-import random
+# def getPersonajeElegido(personaje):
+#     print( personaje.getNombre())
+#     print(bcolors.verde,personaje.getVida(), bcolors.normal)
+#     for i in personaje.getTecnicas():
+#         print("El daño es de ",bcolors.rojo, i.getDano(),bcolors.normal)
 
-def CPU(p1,p2):
-    while True:
-        if p2.getVida() > 0:
-            opcion = random.randint(0,3)
-            if opcion == 0:
-                p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            elif opcion == 1:
-                p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            elif opcion == 2:
-                p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            elif opcion == 3:
-                p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            else:
-                print("no has elegido ningun ataque")
-                break
+# def vidasJugadores(p1,p2):
+#     if p1.getVida() > 0:
+#         print("Vida de tu jugador → ", bcolors.verde, p1.getVida(), bcolors.normal)
+#     else:
+#         print("Vida de tu jugador → ", bcolors.rojo, p1.getVida(), bcolors.normal)
+#     if p2.getVida() > 0:
+#         print("Vida de CPU → ",bcolors.verde, p2.getVida(), bcolors.normal)
+#     else:
+#         print("Vida de CPU → ",bcolors.rojo, p2.getVida(), bcolors.normal)
 
-def personaje1(p1,p2):
-    while True:
-        if p2.getVida() > 0:
-            opcion = int(input("Elige ataque → "))
-            if opcion == 0:
-                p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-            elif opcion == 1:
-                p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-            elif opcion == 2:
-                p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-            elif opcion == 3:
-                p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-            else:
-                print("No has elegido ningún ataque ")
+# def personaje1(p1, p2):
+#     bandera = True
+#     while bandera:
+#         if p2.getVida() > 0:
+#             opcion = int(input("Elige ataque → "))
+#             if opcion == 0:
+#                 p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
+#                 vidasJugadores(p1,p2)
+#                 break
+#             elif opcion == 1:
+#                 p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
+#                 vidasJugadores(p1,p2)
+#                 break
+#             elif opcion == 2:
+#                 p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
+#                 vidasJugadores(p1,p2)
+#                 break
+#             elif opcion == 3:
+#                 p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
+#                 vidasJugadores(p1,p2)
+#                 break
+#             else:
+#                 print("no has elegido ningun ataque")
+#         else:
+            
+#             bandera: False
+        
 
-def lucha(p1,p2):
-    os.system("cls")
-    ronda = 0
-    while p1.getVida() > 0 and p2.getVida() > 0:
-        ronda = ronda + 1
-        for i in range(len(p1.getTecnicas())):
-            print(i, " → ", p1.getTecnicas()[i].getNombre())
-        personaje1(p1,p2)
-        os.system("cls")
-        CPU(p1,p2)
+# def CPU(p1,p2):
+#     bandera = True
+#     while bandera:
+        
+#         if p2.getVida() > 0:
+#             opcion = random.randint(0,3)
+#             if opcion == 0:
+#                 p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
+#                 vidasJugadores(p1,p2)
+#                 break
+#             elif opcion == 1:
+#                 p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
+#                 vidasJugadores(p1,p2)
+#                 break
+#             elif opcion == 2:
+#                 p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
+#                 vidasJugadores(p1,p2)
+#                 break
+#             elif opcion == 3:
+#                 p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
+#                 vidasJugadores(p1,p2)
+#                 break
+#             else:
+#                 print("no has elegido ningun ataque")
+#                 break
+#         else:
+#             bandera = False
+    
 
-def personajeCPU(p1):
-    cpu = random.choice(personajes)
-    os.system("cls")
-    print("Juegas contra ", cpu.getNombre())
-    getPersonajeElegido(cpu)
-    lucha(p1,cpu)
+
+# def lucha(p1,p2):
+#     os.system("cls")
+#     ronda = 0
+#     while p1.getVida() > 0 and p2.getVida() > 0:
+#         ronda = ronda +1 
+#         print("*************************************************")     
+#         print("*                     ROUND ",ronda,"                 *")
+#         print("*************************************************")
+
+#         for i in range(len(p1.getTecnicas())) :
+#             print(i," → ", p1.getTecnicas()[i].getNombre())    
+#         personaje1(p1,p2)
+#         os.system("cls")
+#         CPU(p1,p2)
+#     else:
+#         os.system("cls")
+              
+        
+
+# def personajeCPU(p1):
+#     cpu = random.choice(personajes)
+#     os.system("cls")
+#     print ("Juegas contra ", cpu.getNombre())
+#     getPersonajeElegido(cpu)
+#     lucha(p1,cpu)
 
 
-def vidasJugadores(p1,p2):
-    print("Vda de tu jugador → ", p1.getVida)
-    print("Vida de la CPU → ", p2.getVida)
-
-def getPersonajeElegido(personaje):
-    print(personaje.getNombre())
-    print(personaje.getVida())
-    for i in personaje.getTecnicas():
-        print("El daño es de ", i.getDano())
-
-def confirmacionJugador(personaje):
-    getPersonajeElegido(personaje)
-    opcion = input("¿Quieres este jugador? (s/n) ")
-    if opcion == "s":
-        personajeCPU(personaje)
-    else:
-        os.system("cls")
-        jugar()
+# def confirmacionJufgador(personaje):
+#     getPersonajeElegido(personaje)
+#     opcion = input("¿Quiere este jugador? (s/n) ")
+#     if opcion == "s":
+#         personajeCPU(personaje)
+#     else:
+#         os.system("cls")
+#         jugar()
+    
+# def jugar():
+#     bandera = True
+#     for i in range(len(personajes)):
+#         print (i,personajes[i].getNombre() )
+#     while bandera:
+#         opcion =int(input("Introduce el numero de un personaje → "))
+#         if opcion > len(personajes):
+#             print("Error...")
+#         else:
+#             bandera = False
+#     opcion = personajes[opcion]
+#     print("Tu personaje es ", opcion.getNombre())
+#     os.system("cls")
+#     confirmacionJufgador(opcion)
--- Manu.py
+++ Manu.py
@@ -1,9 +1,8 @@
-from cgitb import reset
+from tokenize import PseudoExtras
+from traceback import print_tb
 from Personaje import Personaje
 import os
 from Tecnicas import Tecnica
-import random
-from colorama import Fore, init,Cursor
 
 personajes =[]
 
@@ -45,124 +44,14 @@ def personajesDefault():
     personajes.append(p3)
     personajes.append(p4)
 
-def getPersonajeElegido(personaje):
-    print(personaje.getNombre())
-    print(personaje.getVida())
-    for i in personaje.getTecnicas():
-        print("El daño es de ",i.getDano())
 
-def vidasJugadores(p1,p2):
-    if p1.getVida() > 0:
-        print("Vida de tu jugador → ", bcolors.verde, p1.getVida(), bcolors.normal)
-    else:
-        print("Vida de tu jugador → ", bcolors.rojo, p1.getVida(), bcolors.normal)
-    if p2.getVida() > 0:
-        print("Vida de CPU → ",bcolors.verde, p2.getVida(), bcolors.normal)
-    else:
-        print("Vida de CPU → ",bcolors.rojo, p2.getVida(), bcolors.normal)
-
-def personaje1(p1, p2):
-    while True:
-        if p2.getVida() > 0:
-            opcion = int(input("Elige ataque → "))
-            if opcion == 0:
-                p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            elif opcion == 1:
-                p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            elif opcion == 2:
-                p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            elif opcion == 3:
-                p2.setVida(p2.getVida() - p1.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            else:
-                print("no has elegido ningun ataque")
-        
-
-def CPU(p1,p2):
-    while True:
-        if p2.getVida() > 0:
-            opcion = random.randint(0,3)
-            if opcion == 0:
-                p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            elif opcion == 1:
-                p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            elif opcion == 2:
-                p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            elif opcion == 3:
-                p1.setVida(p1.getVida() - p2.getTecnicas()[opcion].getDano())
-                vidasJugadores(p1,p2)
-                break
-            else:
-                print("no has elegido ningun ataque")
-                break
-
-def lucha(p1,p2):
-    os.system("cls")
-    ronda = 0
-    while p1.getVida() > 0 and p2.getVida() > 0:
-        ronda = ronda +1 
-        print("*************************************************")
-        print("*                     ROUND ",ronda,"                 *")
-        print("*************************************************")
-
-        for i in range(len(p1.getTecnicas())) :
-            print(i," → ", p1.getTecnicas()[i].getNombre())    
-        personaje1(p1,p2)
-        os.system("cls")
-        CPU(p1,p2)
-        
-        
-
-def personajeCPU(p1):
-    cpu = random.choice(personajes)
-    os.system("cls")
-    print ("Juegas contra ", cpu.getNombre())
-    getPersonajeElegido(cpu)
-    lucha(p1,cpu)
+class bcolors:
+    verde = '\033[92m'
+    rojo = '\033[91m'
+    normal = '\033[0m'
 
 
-def confirmacionJufgador(personaje):
-    getPersonajeElegido(personaje)
-    opcion = input("¿Quiere este jugador? (s/n) ")
-    if opcion == "s":
-        personajeCPU(personaje)
-    else:
-        os.system("cls")
-        jugar()
 
-
-def jugar():
-
-    bandera = True
-    for i in range(len(personajes)):
-        print (i,personajes[i].getNombre() )
-    
-    while bandera:
-        opcion =int(input("Introduce el numero de un personaje → "))
-        if opcion > len(personajes):
-            print("Error...")
-        else:
-            bandera = False
-    opcion = personajes[opcion]
-    print("Tu personaje es ", opcion.getNombre())
-    
-    os.system("cls")
-    confirmacionJufgador(opcion)
-
-    
 def menu():
     salir = False
     opcion = 0
